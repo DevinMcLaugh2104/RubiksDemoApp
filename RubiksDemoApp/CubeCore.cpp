8f38550c5d4ce236875a2c3762e7b7a32d3f2858
Devin McLaughlin <DRMcLaughlin@student.fullsail.edu>
2025-06-11 12:49:22 -0400

    Updated 3D cube into rubiks format and added simple mouse interaction to rotate cube

--- RubiksDemoApp/CubeCore.cpp
+++ RubiksDemoApp/CubeCore.cpp
@@ -1,8 +1,50 @@
 #include "CubeCore.h"
 
 CubeCore::CubeCore() {
-
+    for (int x = 0; x < SIZE; ++x)
+        for (int y = 0; y < SIZE; ++y)
+            for (int z = 0; z < SIZE; ++z) {
+                auto& c = cube[x][y][z];
+                c.transform.setToIdentity();
+                c.transform.translate(x - 1.0f, y - 1.0f, z - 1.0f);
+                c.faceColors = {
+                    QVector3D(1, 1, 1),
+                    QVector3D(1, 1, 0),
+                    QVector3D(0, 0, 1),
+                    QVector3D(0, 1, 0),
+                    QVector3D(1, 0, 0),
+                    QVector3D(1, 0.5, 0)
+                };
+            }
 }
+
+Cubie& CubeCore::accessCubie(int x, int y, int z) {
+    return cube[x][y][z];
+}
+
+const Cubie& CubeCore::getCubie(int x, int y, int z) const {
+    return cube[x][y][z];
+}
+
+void CubeCore::rotateLayer(char axis, int index, bool clockwise) {
+    // Simplified rotation: rotates transforms only
+    QMatrix4x4 rotation;
+    float angle = clockwise ? 90.0f : -90.0f;
+    if (axis == 'x') rotation.rotate(angle, 1, 0, 0);
+    if (axis == 'y') rotation.rotate(angle, 0, 1, 0);
+    if (axis == 'z') rotation.rotate(angle, 0, 0, 1);
+
+    for (int x = 0; x < SIZE; ++x)
+        for (int y = 0; y < SIZE; ++y)
+            for (int z = 0; z < SIZE; ++z) {
+                if ((axis == 'x' && x == index) ||
+                    (axis == 'y' && y == index) ||
+                    (axis == 'z' && z == index)) {
+                    cube[x][y][z].transform = rotation * cube[x][y][z].transform;
+                }
+            }
+}
+
 void CubeCore::moveRightLayer() {
 
 }
--- RubiksDemoApp/CubeCore.h
+++ RubiksDemoApp/CubeCore.h
@@ -1,39 +1,50 @@
 #pragma once
-class CubeCore
-{
-private:
+#include <array>
+#include <QMatrix4x4>
+#include <QVector3D>
 
+struct Cubie {
+    QMatrix4x4 transform;
+    std::array<QVector3D, 6> faceColors; // Up, Down, Left, Right, Front, Back
+};
 
+class CubeCore {
 public:
-	CubeCore();
+    static constexpr int SIZE = 3;
+    Cubie cube[SIZE][SIZE][SIZE];
 
-	//Declare Movement Functions
-	void moveRightLayer();
-	void moveRightLayerPrime();
-	void moveLeftLayer();
-	void moveLeftLayerPrime();
-	void moveUpLayer();
-	void moveUpLayerPrime();
-	void moveDownLayer();
-	void moveDownLayerPrime();
-	void moveFrontLayer();
-	void moveFrontLayerPrime();
-	void moveBackLayer();
-	void moveBackLayerPrime();
-	void moveELayer();
-	void moveELayerPrime();
-	void moveMLayer();
-	void moveMLayerPrime();
-	void moveSLayer();
-	void moveSLayerPrime();
-	void moveRw();
-	void moveRwPrime();
-	void moveLw();
-	void moveLwPrime();
-	void moveX();
-	void moveXPrime();
-	void moveY();
-	void moveYPrime();
-	void moveZ();
-	void moveZPrime();
+    CubeCore();
+    void rotateLayer(char axis, int index, bool clockwise);
+    const Cubie& getCubie(int x, int y, int z) const;
+    Cubie& accessCubie(int x, int y, int z);
+
+    // Original movement functions (keep them for solving logic)
+    void moveRightLayer();
+    void moveRightLayerPrime();
+    void moveLeftLayer();
+    void moveLeftLayerPrime();
+    void moveUpLayer();
+    void moveUpLayerPrime();
+    void moveDownLayer();
+    void moveDownLayerPrime();
+    void moveFrontLayer();
+    void moveFrontLayerPrime();
+    void moveBackLayer();
+    void moveBackLayerPrime();
+    void moveELayer();
+    void moveELayerPrime();
+    void moveMLayer();
+    void moveMLayerPrime();
+    void moveSLayer();
+    void moveSLayerPrime();
+    void moveRw();
+    void moveRwPrime();
+    void moveLw();
+    void moveLwPrime();
+    void moveX();
+    void moveXPrime();
+    void moveY();
+    void moveYPrime();
+    void moveZ();
+    void moveZPrime();
 };
